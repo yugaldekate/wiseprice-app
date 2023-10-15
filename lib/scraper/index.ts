26910213ab8ff1fe166634fa61a8fcfe492846cf
yugaldekate72 <yugal.dekate@ajatus.co.in>
2023-10-15 22:10:03 +0530

    extracted current price, original price, images, outOfStock of product

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -3,6 +3,8 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
+import { extractPrice } from '../utils';
+
 export async function scrapeAmazonProduct(url: string){
     if(!url) return;
 
@@ -33,6 +35,43 @@ export async function scrapeAmazonProduct(url: string){
         
         // Extract the product title
         const title = $('#productTitle').text().trim();
+
+        //extract the product current price
+        const currentPrice = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('.a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+        );
+        
+        //extract the product original price
+        const originalPrice = extractPrice(
+            $('#priceblock_ourprice'),
+            $('.a-price.a-text-price span.a-offscreen'),
+            $('#listPrice'),
+            $('#priceblock_dealprice'),
+            $('.a-size-base.a-color-price')
+        );
+
+        //check the product is outOfStock
+        const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
+         
+        //all images present in a object where key is image url and value is the image dimension array
+        //example {"https://m.media-amazon.com.jpg":[450,450], "https://m.media-amazon.com.jpg":[450,550]}
+        const images = 
+            $('#imgBlkFront').attr('data-a-dynamic-image') || 
+            $('#landingImage').attr('data-a-dynamic-image') ||
+            '{}'
+
+        //take only image object keys i.e., the image url's
+        //example {https://m.media-amazon.com.jpg, https://m.media-amazon.com.jpg}
+        const imageUrls = Object.keys(JSON.parse(images));    
+        
+        console.log("Title --> ", title);
+        console.log("current Price --> ", currentPrice);
+        console.log("original Price --> ", originalPrice);
+        console.log(outOfStock);
+        console.log("Images --> " , images);
+        
         
         
         
---
+++ lib/utils.ts
@@ -0,0 +1,21 @@
+// Extracts and returns the price from a list of possible elements.
+export function extractPrice(...elements: any) {
+    for (const element of elements) {
+        const priceText = element.text().trim();
+    
+        if(priceText) {
+            // removes alphabets from string and keep only numbers with decimals
+            const cleanPrice = priceText.replace(/[^\d.]/g, '');
+    
+            let firstPrice; 
+    
+            if (cleanPrice) {
+                firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+            } 
+    
+            return firstPrice || cleanPrice;
+        }
+    }
+  
+    return '';
+}
