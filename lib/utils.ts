b9954c6b8247136b233fd316b8aae215fb4cd4c0
Yugal D Dekate <yugaldekate72@gmail.com>
2023-10-20 14:00:57 +0530

    created product-details page

--- lib/utils.ts
+++ lib/utils.ts
@@ -86,3 +86,10 @@ export function getAveragePrice(priceList: PriceHistoryItem[]) {
   
     return averagePrice;
 }
+
+export const formatNumber = (num: number = 0) => {
+    return num.toLocaleString(undefined, {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+};
